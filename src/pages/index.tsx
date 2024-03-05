7b69dc17ebc45fc0c45459683d2400fb92cc013a
Meryem İris <irismeryem.96@gmail.com>
2024-03-05 14:47:55 +0300

    install axios and make simple request

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,27 @@
 import Head from "next/head";
+import { useState } from "react";
+
+type Fact = {
+	id: string;
+	text: string;
+};
 
 export default function Home() {
+	const [facts, setFacts] = useState<Fact[]>([]);
+
+	const axios = require("axios");
+
+	const getRandomFact = async () => {
+		try {
+			const response = await axios.get(
+				"https://uselessfacts.jsph.pl/random.json?language=en"
+			);
+			setFacts([...facts, response.data]);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
 	return (
 		<>
 			<Head>
@@ -9,6 +30,15 @@ export default function Home() {
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
+
+			<main>
+				<button onClick={getRandomFact}>Get Random Fact</button>
+				<ul>
+					{facts.map((fact) => (
+						<li key={fact.id}>{fact.text}</li>
+					))}
+				</ul>
+			</main>
 		</>
 	);
 }

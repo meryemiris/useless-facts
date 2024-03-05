7e9ffe396ac781d4da59b53cc9b59fb7bed4ebc9
Meryem İris <irismeryem.96@gmail.com>
2024-03-05 15:45:14 +0300

    add 'en' or 'de' language, and  'random' or 'today' fact type options

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,43 @@
 import Head from "next/head";
 import { useState } from "react";
+import axios from "axios";
 
 type Fact = {
 	id: string;
 	text: string;
 };
 
+type FactType = "random" | "today";
+
 export default function Home() {
-	const [facts, setFacts] = useState<Fact[]>([]);
+	const [randomfact, setRandomFacts] = useState<Fact[]>([]);
+	const [todayfact, setTodayFacts] = useState<Fact[]>([]);
+	const [language, setLanguage] = useState<string>("en");
 
-	const axios = require("axios");
-
-	const getRandomFact = async () => {
+	const getFact = async ({ factType = "random" }: { factType?: FactType }) => {
 		try {
 			const response = await axios.get(
-				"https://uselessfacts.jsph.pl/random.json?language=en"
+				`https://uselessfacts.jsph.pl/${factType}.json?language=${language}`
 			);
-			setFacts([...facts, response.data]);
+
+			if (factType === "random") {
+				setRandomFacts([response.data]);
+			} else {
+				setTodayFacts([response.data]);
+			}
+			console.log(response.data);
 		} catch (error) {
-			console.error(error);
+			console.error("Error fetching data:", error);
 		}
 	};
 
+	const getTodayFact = () => {
+		getFact({ factType: "today" });
+	};
+
+	const getRandomFact = () => {
+		getFact({ factType: "random" });
+	};
 	return (
 		<>
 			<Head>
@@ -30,14 +46,40 @@ export default function Home() {
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-
 			<main>
-				<button onClick={getRandomFact}>Get Random Fact</button>
-				<ul>
-					{facts.map((fact) => (
-						<li key={fact.id}>{fact.text}</li>
-					))}
-				</ul>
+				<select
+					onChange={(e) => setLanguage(e.target.value)}
+					defaultValue={language}
+				>
+					<option value="en">English</option>
+					<option value="de">Deutsch</option>
+				</select>
+				<button onClick={getTodayFact} className="fact-button">
+					Get Fact of the Day
+				</button>
+				<button onClick={getRandomFact} className="fact-button">
+					Get Random Fact
+				</button>
+
+				<div>
+					<h1>Random Fact</h1>
+					<ul>
+						{randomfact.map((fact) => (
+							<li key={fact.id}>{fact.text}</li>
+						))}
+					</ul>
+				</div>
+
+				<div>
+					<h1>Fact of the Day</h1>
+					<ul>
+						{todayfact.length > 0 ? (
+							todayfact.map((fact) => <li key={fact.id}>{fact.text}</li>)
+						) : (
+							<li>No Fact of the Day</li>
+						)}
+					</ul>
+				</div>
 			</main>
 		</>
 	);

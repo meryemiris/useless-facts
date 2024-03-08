d667ae281702218be4b79490ecc30dbc2c8f0000
Meryem İris <irismeryem.96@gmail.com>
2024-03-08 23:44:04 +0300

    adjust random fact styling

--- src/components/randomFact.tsx
+++ src/components/randomFact.tsx
@@ -6,6 +6,9 @@ import { Fact } from "@/pages";
 
 import { GiPerspectiveDiceSixFacesRandom } from "react-icons/gi";
 import { GrCaretNext } from "react-icons/gr";
+import { FaBookmark } from "react-icons/fa";
+import { CiBookmark } from "react-icons/ci";
+import Image from "next/image";
 
 type RandomFactProps = {
 	randomfact: Fact[];
@@ -18,26 +21,19 @@ const RandomFact: React.FC<RandomFactProps> = ({
 	getRandomFact,
 	onBasket,
 }) => {
-	const [clicked, setClicked] = useState(false);
+	const [isSaved, setIsSaved] = useState(false);
 	return (
 		<>
 			{randomfact.length > 0 ? (
 				<div className={styles.card}>
 					<button
-						className={`${styles.saveIcon} ${clicked ? styles.clicked : ""}`}
+						className={`${styles.saveIcon} ${isSaved ? styles.saved : ""}`}
 						onClick={() => {
 							onBasket(randomfact[0]);
-							setClicked(true);
+							setIsSaved(true);
 						}}
 					>
-						<svg
-							viewBox="0 0 384 512"
-							height="1em"
-							xmlns="http://www.w3.org/2000/svg"
-							fill="currentColor"
-						>
-							<path d="M0 48C0 21.5 21.5 0 48 0l0 48V441.4l130.1-92.9c8.3-6 19.6-6 27.9 0L336 441.4V48H48V0H336c26.5 0 48 21.5 48 48V488c0 9-5 17.2-13 21.3s-17.6 3.4-24.9-1.8L192 397.5 37.9 507.5c-7.3 5.2-16.9 5.9-24.9 1.8S0 497 0 488V48z"></path>
-						</svg>
+						{isSaved ? <FaBookmark /> : <CiBookmark />}
 					</button>
 					{randomfact.map((fact) => (
 						<span key={fact.id} className={styles.fact}>
@@ -48,7 +44,7 @@ const RandomFact: React.FC<RandomFactProps> = ({
 					<button
 						onClick={() => {
 							getRandomFact();
-							setClicked(false);
+							setIsSaved(false);
 						}}
 						className={styles.nextButton}
 					>
@@ -57,7 +53,13 @@ const RandomFact: React.FC<RandomFactProps> = ({
 				</div>
 			) : (
 				<button className={styles.button} onClick={getRandomFact}>
-					<GiPerspectiveDiceSixFacesRandom />
+					<Image
+						className={styles.owlImg}
+						src="/randomFact.svg"
+						alt="owl reading book"
+						width={120}
+						height={120}
+					/>
 					Random Fact
 				</button>
 			)}

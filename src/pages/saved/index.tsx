9d026f3b14a1c0fcae0c4e3f49855337ae32493e
Meryem İris <irismeryem.96@gmail.com>
2024-03-09 15:19:08 +0300

    redirect user from saved page if not logged in

--- src/pages/saved/index.tsx
+++ src/pages/saved/index.tsx
@@ -1,17 +1,30 @@
 import Layout from "@/components/Layout";
 import SavedFacts from "@/components/SavedFacts";
+import AuthContext from "@/lib/AuthContext";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useContext, useEffect } from "react";
 
 export default function SavedFactsPage() {
+	const { isLoggedIn } = useContext(AuthContext);
+
+	const router = useRouter();
+
+	useEffect(() => {
+		isLoggedIn ? false : router.push("/login");
+	}, [isLoggedIn, router]);
+
 	return (
 		<>
 			<Head>
 				<title>Saved Facts</title>
 				<meta name="description" content="Saved Facts Page" />
 			</Head>
-			<Layout>
-				<SavedFacts />
-			</Layout>
+			{isLoggedIn && (
+				<Layout>
+					<SavedFacts />
+				</Layout>
+			)}
 		</>
 	);
 }

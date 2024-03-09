51c21ea1934a907895149c1c13285fe95965edf6
Meryem İris <irismeryem.96@gmail.com>
2024-03-10 01:43:30 +0300

    use axios instance

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,3 +1,4 @@
+import Language from "@/components/Language";
 import { Fact } from "@/pages";
 import axios from "axios";
 
@@ -6,13 +7,16 @@ type FetchFactParams = {
   language: "en" | "de";
 };
 
-export async function fetchFact({
-  factType = "random",
-  language = "en",
-}: FetchFactParams) {
-  const response = await axios.get<Fact>(
-    `https://uselessfacts.jsph.pl/${factType}.json?language=${language}`,
-  );
+const instance = axios.create({
+  baseURL: "https://uselessfacts.jsph.pl/api/v2/facts/",
+  timeout: 1000,
+  headers: { Accept: "application/json" },
+});
+
+export async function fetchFact(params: FetchFactParams) {
+  const url = `${params.factType}?language=${params.language}`;
+
+  const response = await instance.get(url);
 
   return response.data;
 }

2fe58fdd2ed9f0ca44bb9870a4adb9b9939341f6
Meryem İris <irismeryem.96@gmail.com>
2024-03-09 18:23:22 +0300

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
-/** @type {import('next').NextConfig} */
+import path from "path";
+
 const nextConfig = {
-  reactStrictMode: true,
+	reactStrictMode: true,
+	webpack: (config) => {
+		config.resolve.alias["@"] = path.resolve(__dirname, "src");
+		return config;
+	},
 };
 
 export default nextConfig;

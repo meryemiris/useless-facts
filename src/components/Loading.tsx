46956deff1fcbaf9325b3dfcd3840e70988ed7d0
Meryem İris <irismeryem.96@gmail.com>
2024-03-10 16:52:49 +0300

    change Loading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,16 +2,99 @@ import styles from "./Loading.module.css";
 
 export default function Loading() {
   return (
-    <div className={styles.loader}>
-      <span className={styles.l}>L</span>
-      <span className={styles.o}>o</span>
-      <span className={styles.a}>a</span>
-      <span className={styles.d}>d</span>
-      <span className={styles.i}>i</span>
-      <span className={styles.n}>n</span>
-      <span className={styles.g}>g</span>
-      <span className={styles.d1}>.</span>
-      <span className={styles.d2}>.</span>
+    <div>
+      <svg
+        height="108px"
+        width="108px"
+        viewBox="0 0 128 128"
+        className={styles.loader}
+      >
+        <defs>
+          <clipPath id="loader-eyes">
+            <circle
+              transform="rotate(-40,64,64) translate(0,-56)"
+              r="8"
+              cy="64"
+              cx="64"
+              className={styles.loaderEye1}
+            ></circle>
+            <circle
+              transform="rotate(40,64,64) translate(0,-56)"
+              r="8"
+              cy="64"
+              cx="64"
+              className={styles.loaderEye2}
+            ></circle>
+          </clipPath>
+          <linearGradient y2="1" x2="0" y1="0" x1="0" id="loader-grad">
+            <stop stop-color="#000" offset="0%"></stop>
+            <stop stop-color="#fff" offset="100%"></stop>
+          </linearGradient>
+          <mask id="loader-mask">
+            <rect
+              fill="url(#loader-grad)"
+              height="128"
+              width="128"
+              y="0"
+              x="0"
+            ></rect>
+          </mask>
+        </defs>
+        <g
+          stroke-dasharray="175.93 351.86"
+          stroke-width="12"
+          stroke-linecap="round"
+        >
+          <g>
+            <rect
+              clip-path="url(#loader-eyes)"
+              height="64"
+              width="128"
+              fill="#f08080"
+            ></rect>
+            <g stroke="#f08080" fill="none">
+              <circle
+                transform="rotate(180,64,64)"
+                r="56"
+                cy="64"
+                cx="64"
+                className={styles.loaderMouth1}
+              ></circle>
+              <circle
+                transform="rotate(0,64,64)"
+                r="56"
+                cy="64"
+                cx="64"
+                className={styles.loaderMouth2}
+              ></circle>
+            </g>
+          </g>
+          <g mask="url(#loader-mask)">
+            <rect
+              clip-path="url(#loader-eyes)"
+              height="64"
+              width="128"
+              fill="rgb(255, 201, 201)"
+            ></rect>
+            <g stroke="rgb(255, 201, 201)" fill="none">
+              <circle
+                transform="rotate(180,64,64)"
+                r="56"
+                cy="64"
+                cx="64"
+                className={styles.loaderMouth1}
+              ></circle>
+              <circle
+                transform="rotate(0,64,64)"
+                r="56"
+                cy="64"
+                cx="64"
+                className={styles.loaderMouth2}
+              ></circle>
+            </g>
+          </g>
+        </g>
+      </svg>
     </div>
   );
 }
--- src/components/SavedFacts.tsx
+++ src/components/SavedFacts.tsx
@@ -20,13 +20,12 @@ const SavedFacts = () => {
   const { setActivePage } = useFactContext();
 
   const [savedFacts, setSavedFacts] = useState<SavedFacts>([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     if (userId) {
       const getSavedFacts = async () => {
         try {
-          setIsLoading(true);
           let { data, error } = await supabase
             .from("facts")
             .select("*")
@@ -35,6 +34,8 @@ const SavedFacts = () => {
           setSavedFacts(data as SavedFacts);
         } catch (error) {
           console.log(error);
+        } finally {
+          setIsLoading(false);
         }
       };
       getSavedFacts();
@@ -57,82 +58,74 @@ const SavedFacts = () => {
     }
   };
 
-  return (
+  return isLoading ? (
+    <Loading />
+  ) : savedFacts?.length > 0 ? (
     <ul className={styles.facts}>
-      {savedFacts?.length > 0 ? (
-        savedFacts?.map((fact) => (
-          <li className={styles.fact} key={fact.id}>
-            <Image
-              className={styles.modalImg}
-              src={"/fact.svg"}
-              width={40}
-              height={40}
-              alt={"fact modal image"}
-            />
-            <p> {fact.content}</p>
-            <button
-              className={styles.binButton}
-              onClick={() => handleDeleteFact(fact.id)}
+      {savedFacts?.map((fact) => (
+        <li className={styles.fact} key={fact.id}>
+          <Image
+            className={styles.modalImg}
+            src={"/fact.svg"}
+            width={40}
+            height={40}
+            alt={"fact modal image"}
+          />
+          <p> {fact.content}</p>
+          <button
+            className={styles.binButton}
+            onClick={() => handleDeleteFact(fact.id)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 39 7"
+              className={styles.binTop}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 39 7"
-                className={styles.binTop}
-              >
-                <line
-                  strokeWidth="4"
-                  stroke="white"
-                  y2="5"
-                  x2="39"
-                  y1="5"
-                ></line>
-                <line
-                  strokeWidth="3"
-                  stroke="white"
-                  y2="1.5"
-                  x2="26.0357"
-                  y1="1.5"
-                  x1="12"
-                ></line>
-              </svg>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 33 39"
-                className={styles.binBottom}
-              >
-                <mask fill="white" id="path-1-inside-1_8_19">
-                  <path d="M0 0H33V35C33 37.2091 31.2091 39 29 39H4C1.79086 39 0 37.2091 0 35V0Z"></path>
-                </mask>
-                <path
-                  mask="url(#path-1-inside-1_8_19)"
-                  fill="white"
-                  d="M0 0H33H0ZM37 35C37 39.4183 33.4183 43 29 43H4C-0.418278 43 -4 39.4183 -4 35H4H29H37ZM4 43C-0.418278 43 -4 39.4183 -4 35V0H4V35V43ZM37 0V35C37 39.4183 33.4183 43 29 43V35V0H37Z"
-                ></path>
-                <path strokeWidth="4" stroke="white" d="M12 6L12 29"></path>
-                <path strokeWidth="4" stroke="white" d="M21 6V29"></path>
-              </svg>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 89 80"
-                className={styles.garbage}
-              >
-                <path
-                  fill="white"
-                  d="M20.5 10.5L37.5 15.5L42.5 11.5L51.5 12.5L68.75 0L72 11.5L79.5 12.5H88.5L87 22L68.75 31.5L75.5066 25L86 26L87 35.5L77.5 48L70.5 49.5L80 50L77.5 71.5L63.5 58.5L53.5 68.5L65.5 70.5L45.5 73L35.5 79.5L28 67L16 63L12 51.5L0 48L16 25L22.5 17L20.5 10.5Z"
-                ></path>
-              </svg>
-            </button>
-          </li>
-        ))
-      ) : isLoading ? (
-        <Loading />
-      ) : (
-        <p>Start saving interesting facts! You have not saved any facts yet.</p>
-      )}
+              <line strokeWidth="4" stroke="white" y2="5" x2="39" y1="5"></line>
+              <line
+                strokeWidth="3"
+                stroke="white"
+                y2="1.5"
+                x2="26.0357"
+                y1="1.5"
+                x1="12"
+              ></line>
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 33 39"
+              className={styles.binBottom}
+            >
+              <mask fill="white" id="path-1-inside-1_8_19">
+                <path d="M0 0H33V35C33 37.2091 31.2091 39 29 39H4C1.79086 39 0 37.2091 0 35V0Z"></path>
+              </mask>
+              <path
+                mask="url(#path-1-inside-1_8_19)"
+                fill="white"
+                d="M0 0H33H0ZM37 35C37 39.4183 33.4183 43 29 43H4C-0.418278 43 -4 39.4183 -4 35H4H29H37ZM4 43C-0.418278 43 -4 39.4183 -4 35V0H4V35V43ZM37 0V35C37 39.4183 33.4183 43 29 43V35V0H37Z"
+              ></path>
+              <path strokeWidth="4" stroke="white" d="M12 6L12 29"></path>
+              <path strokeWidth="4" stroke="white" d="M21 6V29"></path>
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 89 80"
+              className={styles.garbage}
+            >
+              <path
+                fill="white"
+                d="M20.5 10.5L37.5 15.5L42.5 11.5L51.5 12.5L68.75 0L72 11.5L79.5 12.5H88.5L87 22L68.75 31.5L75.5066 25L86 26L87 35.5L77.5 48L70.5 49.5L80 50L77.5 71.5L63.5 58.5L53.5 68.5L65.5 70.5L45.5 73L35.5 79.5L28 67L16 63L12 51.5L0 48L16 25L22.5 17L20.5 10.5Z"
+              ></path>
+            </svg>
+          </button>
+        </li>
+      ))}
     </ul>
+  ) : (
+    <p className={styles.noFacts}>No facts saved yet</p>
   );
 };
 

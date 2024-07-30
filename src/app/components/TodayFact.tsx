deb6ebbac5ff834ae9850b8a1fd19dc5b7f3af22
Meryem İris <irismeryem.96@gmail.com>
2024-07-30 20:41:29 +0300

    create modal component

--- src/app/components/TodayFact.tsx
+++ src/app/components/TodayFact.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 import Image from "next/image";
 
@@ -10,11 +10,11 @@ import { fetchFact } from "@/app/lib/api";
 import { useFactContext } from "@/app/lib/FactContext";
 
 import { IoBag, IoBagOutline, IoClose } from "react-icons/io5";
-import useClickOutside from "@/app/lib/useClickOutside";
 
 import { toast } from "sonner";
 import { Fact } from "@/app/lib/types";
 import CardWithButtons from "../ui/CardWithButtons";
+import Modal from "../ui/Modal";
 
 const TodayFact = () => {
   const { factBasket, addToBasket, removeFromBasket, language } =
@@ -22,12 +22,6 @@ const TodayFact = () => {
 
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [todayFact, setTodayFact] = useState<Fact>();
-
-  const modalRef = useRef<HTMLDivElement>(null);
-
-  const handleClickOutside = useCallback(() => setIsModalOpen(false), []);
-
-  useClickOutside(modalRef, handleClickOutside, isModalOpen);
 
   useEffect(() => {
     fetchFact({
@@ -59,31 +53,26 @@ const TodayFact = () => {
         <span className={styles.factText}>fact</span>
         <span className={styles.todayText}>today</span>
       </button>
-
-      {isModalOpen && (
-        <div className={styles.modal}>
-          <div ref={modalRef} className={styles.modalContent}>
-            {todayFact ? (
-              <CardWithButtons
-                isHeader
-                onClose={() => setIsModalOpen(false)}
-                key={todayFact.id}
-                onPrimaryAction={() => {
-                  todayFact && isInBasket
-                    ? removeFromBasket(todayFact.id)
-                    : addToBasket(todayFact);
-                }}
-                onSecondaryAction={() => setIsModalOpen(false)}
-                primaryIcon={isInBasket ? <IoBag /> : <IoBagOutline />}
-                primaryLabel={isInBasket ? "Drop from Basket" : "Add to Basket"}
-                secondaryIcon={<IoClose />}
-                secondaryLabel="Close"
-                content={todayFact.text}
-              />
-            ) : null}
-          </div>
-        </div>
-      )}
+      <Modal isOpen={isModalOpen} setIsOpen={setIsModalOpen}>
+        {todayFact && (
+          <CardWithButtons
+            isHeader
+            onClose={() => setIsModalOpen(false)}
+            key={todayFact.id}
+            onPrimaryAction={() => {
+              todayFact && isInBasket
+                ? removeFromBasket(todayFact.id)
+                : addToBasket(todayFact);
+            }}
+            onSecondaryAction={() => setIsModalOpen(false)}
+            primaryIcon={isInBasket ? <IoBag /> : <IoBagOutline />}
+            primaryLabel={isInBasket ? "Drop from Basket" : "Add to Basket"}
+            secondaryIcon={<IoClose />}
+            secondaryLabel="Close"
+            content={todayFact.text}
+          />
+        )}
+      </Modal>
     </>
   );
 };
---
+++ src/app/ui/Modal.tsx
@@ -0,0 +1,33 @@
+import { useCallback, useRef } from "react";
+
+import styles from "./Modal.module.css";
+
+import useClickOutside from "@/app/lib/useClickOutside";
+
+type Props = {
+  children: React.ReactNode;
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const Modal: React.FC<Props> = ({ children, isOpen, setIsOpen }) => {
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  const handleClickOutside = useCallback(() => setIsOpen(false), [setIsOpen]);
+
+  useClickOutside(modalRef, handleClickOutside, isOpen);
+
+  return (
+    <>
+      {isOpen && (
+        <div className={styles.modal}>
+          <div ref={modalRef} className={styles.modalContent}>
+            {children}
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Modal;

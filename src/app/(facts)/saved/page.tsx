ba496120747c1252d95e5eb8cbb10301c88aae75
Meryem İris <irismeryem.96@gmail.com>
2024-07-31 14:11:17 +0300

    add redirect to saved page

--- src/app/(facts)/saved/page.tsx
+++ src/app/(facts)/saved/page.tsx
@@ -1,5 +1,13 @@
+import { redirect } from "next/navigation";
 import SavedFacts from "./components/SavedFacts";
+import { createClient } from "@/utils/supabase/server";
 
-export default function SavedFactsPage() {
+export default async function SavedFactsPage() {
+  const supabase = createClient();
+  const { data, error } = await supabase.auth.getUser();
+  console.log(data);
+  if (error || !data?.user) {
+    redirect("/login");
+  }
   return <SavedFacts />;
 }
